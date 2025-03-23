ccdea74195fc9db7f0f8cd51390fc2e4405d063f
Non0reo <pieroni.nolhan@gmail.com>
2025-03-24 00:28:16 +0100

    Added text prototype

--- src/js/scene/rainbows/svg.js
+++ src/js/scene/rainbows/svg.js
@@ -1,19 +1,23 @@
 let rainbowLines = document.querySelectorAll(".rainbow-line");
+const textRainbow = document.querySelector("#text-rainbow0");
 
-rainbowLines.forEach((line) => {
+rainbowLines.forEach((line, i) => {
     console.dir(line);
     console.log(line.getTotalLength());
 
     line.style.strokeDasharray = line.getTotalLength();
     line.style.strokeDashoffset = line.getTotalLength();
+    line.id = "rainbow-line" + i;
 });
 
 
 function setLines() {
 
-    const mappedProgress = progress * 100;
 
     rainbowLines.forEach((line) => {
         line.style.strokeDashoffset = line.getTotalLength() - (line.getTotalLength() * progress);
+        //textRainbow.setAttribute('startOffset', line.getTotalLength() * progress * 2);
+        textRainbow.setAttribute('textLength', line.getTotalLength() * progress * 2);
+        //textRainbow.textContent = Math.round(progress * 100) + "%";
     });
 }
