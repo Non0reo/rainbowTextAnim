8ae6d8c15115e9fb3b8c39e2470eaf76e97470b4
Non0reo <pieroni.nolhan@gmail.com>
2025-04-14 20:38:31 +0200

    Reworked word throw and background image

--- src/js/scene/words/throw.js
+++ src/js/scene/words/throw.js
@@ -1,9 +1,34 @@
-const image = document.getElementById('image');
+const imageObject = document.getElementById('ccss');
+const image = imageObject.contentDocument.getElementById('human');
+
 const container = document.getElementById('container');
 const words = ["Woaw", "Man !", "Wow", "Whoa", "Woahohoho", "OMG", "Wooooooow", "Aaaaaaahaaahaaaa", "Woawwwwww", "Hoooooo", "Yeaaaaaah", "Look at that"];
 
 image.addEventListener('click', () => {
+    console.log("click");
+
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    const startPosition = screenHeight / 2;
     
+    //new Word("Woaw", startPosition, endPosition(), 1000);
+
+    /* for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        const l1 = startPosition;
+        const l2 = startPosition + (Math.random() < 0.5 ? -1 : 1) * ((Math.random() * screenWidth * 3 / 4) + screenWidth / 4);
+        const c = Math.random() * screenHeight / 2 + screenHeight / 4; // Valeur plus grande pour une parabole plus haute
+
+        new Word(word, l1, l2, c);
+    } */
+
+    new Word(
+        words[Math.floor(Math.random() * words.length)],
+        startPosition,
+        startPosition + (Math.random() < 0.5 ? -1 : 1) * ((Math.random() * screenWidth * 3 / 4) + screenWidth / 4),
+        Math.random() * screenHeight / 2 + screenHeight / 4
+    );
 });
 
 
@@ -16,6 +41,11 @@ class Word {
     */
     constructor(word, l1, l2, c) {
         this.t = 0;
+        this.lastX = 0;
+        this.lastY = 0;
+        this.x = 0;
+        this.y = 0;
+        this.yOffset = window.innerHeight - image.getBoundingClientRect().top;
         this.word = word;
         this.l1 = l1;
         this.l2 = l2;
@@ -24,34 +54,51 @@ class Word {
         this.element = document.createElement('span');
         this.element.className = 'word';
         this.element.textContent = word;
-        container.appendChild(span);
+        container.appendChild(this.element);
 
         this.deltaL = l2 - l1;  
 
         this.a = -4 * c / (this.deltaL ** 2); // Moins de valeur négative pour éviter d'être trop serré
-        this.delta = -4 * a * c;
+        this.delta = -4 * this.a * c;
 
-        this.p = this.deltaL > 0 ? Math.sqrt(this.delta) / 2 * a : -Math.sqrt(this.delta) / 2 * a; // deltaL > 0 => parabola vers le bas
+        this.p = (this.deltaL > 0 ? 1 : -1) * Math.sqrt(this.delta) / (2 * this.a); // deltaL > 0 => parabola vers le bas
+
+        this.animate();
     }
 
 
     calculateY(x) {
-        return a * (x + this.p - this.l1) ** 2 + this.c; // y commence en bas et monte
+        return this.a * (x + this.p - this.l1) ** 2 + this.c; // y commence en bas et monte
     }
 
     animate() {
-        this.t += 0.01;
+        this.t += 0.004;
+        //this.t += 0.001;
 
-        const x = this.t * this.deltaL + this.l1; // va de -l à +l
-        const y = this.calculateY(x);
+        this.x = this.t * this.deltaL + this.l1; // va de -l à +l
+        this.y = this.calculateY(this.x);
 
-        this.element.style.left = `${x}px`;
-        this.element.style.bottom = `${y}px`;
+        this.element.style.left = `${this.x}px`;
+        this.element.style.bottom = `${this.y + this.yOffset}px`;
 
-        if (t < 1) {
-            requestAnimationFrame(animate);
+        //rotate word using lastX and lastY
+        const dx = this.x - this.lastX;
+        const dy = this.y - this.lastY;
+        const angle = -Math.atan2(dy, dx) * 180 / Math.PI + (this.deltaL > 0 ? 0 : 180);
+        this.element.style.transform = `rotate(${angle}deg)`;
+
+        this.lastX = this.x;
+        this.lastY = this.y;
+        this.element.style.opacity = 1 - this.t; // Fade out effect
+
+        if (this.t < 1) {
+            window.requestAnimationFrame(this.animate.bind(this));
         } else {
-            this.element.style.opacity = 0;
+            this.remove();
         }
+    }
+
+    remove() {
+        this.element.remove();
     }
 }
